e95c08ff004cfd965b6575fea3d3cfcc5c9bebae
jmello <juka42@gmail.com>
2020-10-13 17:07:06 -0300

    Add arguments using argument parser

--- workspace/get_input_args.py
+++ workspace/get_input_args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
-#          from the user using the Argparse Python module. If the user fails to 
+#
+# PROGRAMMER:
+# DATE CREATED:
+# REVISED DATE:
+# PURPOSE: Create a function that retrieves the following 3 command line inputs
+#          from the user using the Argparse Python module. If the user fails to
 #          provide some or all of the 3 inputs, then the default values are
 #          used for the missing inputs. Command Line Arguments:
 #     1. Image Folder as --dir with default value 'pet_images'
@@ -18,16 +18,16 @@
 import argparse
 
 # TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
+#       in the return statement with parser.parse_args() parsed argument
 #       collection that you created with this function
-# 
+#
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
+    they run the program from a terminal window. This function uses Python's
+    argparse module to created and defined these 3 command line arguments. If
+    the user fails to provide some or all of the 3 arguments, then the default
+    values are used for the missing arguments.
     Command Line Arguments:
       1. Image Folder as --dir with default value 'pet_images'
       2. CNN Model Architecture as --arch with default value 'vgg'
@@ -36,13 +36,25 @@ def get_input_args():
     Parameters:
      None - simply using argparse module to create & store command line arguments
     Returns:
-     parse_args() -data structure that stores the command line arguments object  
+     parse_args() -data structure that stores the command line arguments object
     """
     # Create Parse using ArgumentParser
-    
+    ap = argparse.ArgumentParser()
     # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+    ap.add_argument("-d", "--dir", required=False,
+                    help="Image folder", default = "pet_images/")
+    ap.add_argument("-a", "--arch", required=False,
+                    help="CNN model architecture (resnet, alexnet, or vgg)",
+                    default = "vgg")
+    ap.add_argument("-f", "--dogfile",
+                    required=False, help="Text File with Dog Names",
+                    default = "dognames.txt")
+
+    args = ap.parse_args()
+    print("Argument 1 - folder: {}".format(args.dir))
+    print("Argument 2 - CNN model architecture: {}".format(args.arch))
+    print("Argument 3 - file with dog names: {}".format(args.dogfile))
+
+    # Replace None with parser.parse_args() parsed argument collection that
+    # you created with this function
+    return ap.parse_args()
